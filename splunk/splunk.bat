9d8de55a3d3a63cd207054b2413f0e266fdfb6b0
echo8358 <codykhiggins@gmail.com>
2024-10-31 20:44:06 -0600

    Actually added files

---
+++ splunk/splunk.bat
@@ -0,0 +1,1 @@
+rem Install a version of splunk compatible with Windows XP (I think 5 works)
---
+++ windows/hardening/Local-Hardening.bat
@@ -0,0 +1,4 @@
+rem Redo the local hardening powershell script in batch
+
+
+rem Install powershell 2 (XP) or 5.1 (7)?
