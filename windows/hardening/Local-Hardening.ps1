592207af8dbd870e840ae7f27e30fe8e7a46e5dc
echo8358 <codykhiggins@gmail.com>
2025-01-01 03:28:47 -0700

    second to last change

--- windows/hardening/Local-Hardening.ps1
+++ windows/hardening/Local-Hardening.ps1
@@ -446,7 +446,7 @@ function Download-Install-Setup-Splunk {
         if ((Get-ChildItem ./splunk.ps1).Length -lt 6000) {
             ./splunk.ps1 $Version $SplunkServer
         } else {
-            ./splunk.ps1 $Version $SplunkServer "dc"
+            ./splunk.ps1 $Version $SplunkServer "member"
         }
 
     } catch {
