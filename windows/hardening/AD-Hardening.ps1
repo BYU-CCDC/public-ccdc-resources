bc1e269d92d284897047d39edca4260057b06221
echo8358 <codykhiggins@gmail.com>
2025-04-11 15:36:53 -0600

    changes

--- windows/hardening/AD-Hardening.ps1
+++ windows/hardening/AD-Hardening.ps1
@@ -1786,9 +1786,9 @@ function Change-DA-Passwords {
                     $writer.write("$pwPlainText$($_.name)")
                     $writer.Flush()
                     $stringAsStream.Position = 0
-                    $newpw = (Get-FileHash -Algorithm SHA256 -InputStream $stringAsStream).hash + "!"
+                    $newpw = (Get-FileHash -Algorithm SHA256 -InputStream $stringAsStream).hash
                     write-host $newpw.Substring(0,12)
-                    $_ | Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText -String $newpw.Substring(0,12) -Force)
+                    $_ | Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText -String ($newpw.Substring(0,12) + "!") -Force)
                 }
                 Write-Host "Success!!`n"
 
@@ -1829,9 +1829,9 @@ function Change-User-Passwords {
                     $writer.write("$pwPlainText$($_.name)")
                     $writer.Flush()
                     $stringAsStream.Position = 0
-                    $newpw = (Get-FileHash -Algorithm SHA256 -InputStream $stringAsStream).hash + "!"
+                    $newpw = (Get-FileHash -Algorithm SHA256 -InputStream $stringAsStream).hash
                     write-host $newpw.Substring(0,12)
-                    $_ | Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText -String $newpw.Substring(0,12) -Force)
+                    $_ | Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText -String ($newpw.Substring(0,12) + "!") -Force)
                 }
                 Write-Host "Success!!`n"
 
